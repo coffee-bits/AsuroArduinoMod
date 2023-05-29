8038d8d86964709b6431522ee3602ab38a356502
coffee-bits <christian.jess.mobil@gmail.com>
2023-05-29 22:24:00 +0200

    Licence Statement from comment removed

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,7 @@
 /****************************************************************************
  *   This code is distributed in the hope that it will be useful,           *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *   GNU Lesser General Public License for more details.                    *
- *                                                                          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                   *
  ****************************************************************************/
 
 /**
